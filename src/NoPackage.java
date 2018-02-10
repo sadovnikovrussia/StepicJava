f22669cb5dee4e7f8abaad1e06ad10927bb7d475
sadovnikovrussia <31988317+sadovnikovrussia@users.noreply.github.com>
2018-02-10 12:20:43 +0300

    oop

---
+++ src/NoPackage.java
@@ -0,0 +1,5 @@
+public class NoPackage {
+    public static void main(){
+        System.out.println("nopackage");
+    }
+}
---
+++ src/sadovnikov/oop/OOP.java
@@ -0,0 +1,7 @@
+package sadovnikov.oop;
+
+public class OOP {
+    public static void main(String[] args) {
+        System.out.println("OOP");
+    }
+}
